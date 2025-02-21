9ce57a3c52d8b030d054c7a3c6fa5b2d2eac9fef
ArmandoPro25 <ruanoarmando54@gmail.com>
2025-02-21 09:43:12 -0600

    API

--- API_Usuarios/WeatherForecast.cs
+++ API_Usuarios/WeatherForecast.cs
@@ -4,16 +4,16 @@ namespace API_Usuarios
     {
         public int Id { get; set; }
 
-        public string Nombre_Completo { get; set; }
+        public string? Nombre_Completo { get; set; }
         
-        public string Nombre_Usuario { get; set; }
+        public string? Nombre_Usuario { get; set; }
         
-        public string Correo_Electronico { get; set; }
+        public string? Correo_Electronico { get; set; }
         
         public char Sexo { get; set; }
 	    
         public int Numero_Telefono { get; set; }
         
-        public string Contrasena { get; set; }
+        public string? Contrasena { get; set; }
     }
 }
